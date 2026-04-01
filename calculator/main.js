aecfa456af2f94886a463edea3747d33bc636f7f
alincqeek <pstartceva@yandex.ru>
2026-04-01 08:49:10 +0300

    Initial commit

--- calculator/main.js
+++ calculator/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
+
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear");
